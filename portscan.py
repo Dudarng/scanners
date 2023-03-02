17e0be240347a39b26082fd349cefe26db82373b
Maria Eduarda <76593812+Dudarng@users.noreply.github.com>
2023-03-02 12:43:53 -0300

    Add files via upload

---
+++ portscan.py
@@ -0,0 +1,29 @@
+import socket
+import sys
+
+def scan(host, ports):
+    try:
+        for port in ports:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.settimeout(0.5)
+            code  = client.connect_ex((host, int(port)))
+            if code == 0:
+                print("[+] {} open".format(port))
+        
+    except:
+        print("Error, something is wrong!")
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        host = sys.argv[1]
+        if (len(sys.argv) >= 3):
+            ports = sys.argv[2].split(",")
+        else:
+            ports = [21, 22, 23, 25, 80, 443, 8080, 8443, 3386, 139, 135]
+
+        scan(host, ports)
+
+        print("Quer testar um range especifico?")
+
+    else:
+        print("Usage: python portscan.py google.com 22,23,80,443")
